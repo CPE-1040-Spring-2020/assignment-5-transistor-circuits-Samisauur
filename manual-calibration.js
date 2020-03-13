9f378f13470e657602d6da926327190394901a43
Samisauur <60164690+Samisauur@users.noreply.github.com>
2020-03-12 22:51:28 -0600

    Create manual-calibration.js

---
+++ manual-calibration.js
@@ -0,0 +1,19 @@
+let value1 = pins.analogReadPin(AnalogPin.P0)
+let index = Math.map(value1, 6, 1023, 4, 0)
+
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    value1 = pins.analogReadPin(AnalogPin.P0)
+    index = Math.map(value1, 6, 1023, 4, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.clearScreen()
+    for (let y = 4; y >= index; y--) {
+        led.plot(0, y)
+        led.plot(1, y)
+        led.plot(2, y)
+        led.plot(3, y)
+        led.plot(4, y)
+    }
+    basic.pause(200)
+    basic.clearScreen()
+})
