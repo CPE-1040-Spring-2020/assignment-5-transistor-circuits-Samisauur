ae0c3c9648bb7d54c6e1598e2d5b873165d5dd7c
Samisauur <60164690+Samisauur@users.noreply.github.com>
2020-03-20 13:31:35 -0600

    Update manual-calibration.js

--- manual-calibration.js
+++ manual-calibration.js
@@ -1,10 +1,10 @@
 let value1 = pins.analogReadPin(AnalogPin.P0)
-let index = Math.map(value1, 6, 1023, 4, 0)
+let index = Math.map(value1, 0, 1023, 0, 4)
 
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P16, 1)
     value1 = pins.analogReadPin(AnalogPin.P0)
-    index = Math.map(value1, 6, 1023, 4, 0)
+    index = Math.map(value1, 0, 1023, 0, 4)
     pins.digitalWritePin(DigitalPin.P16, 0)
     basic.clearScreen()
     for (let y = 4; y >= index; y--) {
